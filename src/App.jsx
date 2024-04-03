d889e24e031db75ea73c440db3bcb90bbe39e3b8
tonnyvijayan <tonnyvijayan@gmail.com>
2024-04-03 21:15:00 +0530

    feat:added top navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Login } from "./components/Login";
 
 import { Routes, Route } from "react-router-dom";
 import { SignUp } from "./components/SignUp";
+import { TopNavBar } from "./components/TopNavBar";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<TopNavBar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
