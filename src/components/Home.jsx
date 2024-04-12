cef4c3d9cb9226171c9b066063e09d53806ad83d
tonnyvijayan <tonnyvijayan@gmail.com>
2024-04-12 21:04:02 +0530

    chores:cleaned up nav code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import { SideNavBar } from "./SideNavBar";
 import { TopNavBar } from "./TopNavBar";
+// import { ListingArea } from "./ListingArea";
 
 export const Home = () => {
   return (
     <>
       <TopNavBar />
+      {/* <ListingArea /> */}
       <SideNavBar />
     </>
   );
---
+++ src/components/ListingArea.jsx
@@ -0,0 +1,11 @@
+import "./ListingArea.css";
+
+export const ListingArea = () => {
+  return (
+    <>
+      <div className="listing-div">
+        <h1>welcome to video listing</h1>
+      </div>
+    </>
+  );
+};
