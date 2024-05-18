9c13a86c0f7b3ac76aac7f794d88edc9f6c9594b
tonnyvijayan <tonnyvijayan@gmail.com>
2024-05-18 21:29:11 +0530

    refactor:cleaned up useaxioshook

--- src/hooks/useAxiosPrivate.jsx
+++ src/hooks/useAxiosPrivate.jsx
@@ -11,17 +11,12 @@ export const useAxiosPrivate = () => {
   useEffect(() => {
     const requestInterceptor = axiosPrivate.interceptors.request.use(
       (config) => {
-        console.log("from config");
         if (!config.headers["Authorization"]) {
-          console.log("from config auth missin");
-
           config.headers["Authorization"] = `Bearer ${authState}`;
-          console.log("old token", authState);
         }
         return config;
       },
       (error) => {
-        console.log("from request error", error);
         return Promise.reject(error);
       }
     );
@@ -29,14 +24,12 @@ export const useAxiosPrivate = () => {
     const responseInterceptor = axiosPrivate.interceptors.response.use(
       (response) => response,
       async (error) => {
-        console.log("response error", error);
         const previousRequest = error?.config;
-        console.log("before setting header", previousRequest);
+
         if (error?.response?.status === 403 && !previousRequest?.sent) {
           previousRequest.sent = true;
           let newAccessToken = await refresh();
           previousRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-          console.log("after setting header", previousRequest);
 
           return axiosPrivate(previousRequest);
         }
